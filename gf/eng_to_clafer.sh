cb0868d6b41c0ee3b3a83732a8de2cef5447808f
Alexander Bakst <abakst@galois.com>
2020-03-19 17:09:19 -0700

    Add a tempdir for gfo files

--- gf/eng_to_clafer.sh
+++ gf/eng_to_clafer.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+tmp_dir=$(mktemp -d -t eng_to_clafer_XXXXXXX)
+
 read INPUT
 CMD="ps -lextext \"$INPUT\" | parse -lang=InfoLeakageEng | linearize -lang=InfoLeakageClafer"
 DIR=`dirname $0`
-echo $CMD | gf --run $DIR/InfoLeakageEng.gf $DIR/InfoLeakageClafer.gf
+echo $CMD | gf --gfo-dir=${tmp_dir} --run $DIR/InfoLeakageEng.gf $DIR/InfoLeakageClafer.gf
+
+rm -rf $tmp_dir
