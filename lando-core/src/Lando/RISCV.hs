2693fca2925b761096306e4fb663caf225033f5f
Ben Selfridge <benselfridge@galois.com>
2020-05-24 11:16:51 -0700

    stubbed out Piccolo backend

--- lando-core/src/Lando/RISCV.hs
+++ lando-core/src/Lando/RISCV.hs
@@ -1,4 +1,6 @@
-module Lando.Backends.BSC
+-- | This module defines the feature model for the Bluespec compiler Piccolo
+-- backend.
+module Lando.RISCV
   ( RV(..)
   , RegWidth(..)
   , Extensions(..)
@@ -9,10 +11,13 @@ module Lando.Backends.BSC
   , CExt(..)
   ) where
 
+-- | The RISC-V feature model.
 data RV = RV RegWidth Extensions
 
+-- | Register widths supported by RISC-V
 data RegWidth = RV32 | RV64
 
+-- | ISA extensions (on top of the base ISA)
 data Extensions = Extensions { mExt :: MExt
                              , aExt :: AExt
                              , fExt :: FExt
@@ -20,9 +25,13 @@ data Extensions = Extensions { mExt :: MExt
                              , cExt :: CExt
                              }
 
+-- | The M (multiply) extension
 data MExt = MYes | MNo
+-- | The A (memory atomics) extension
 data AExt = AYes | ANo
+-- | The F (single-precision floating point) extension
 data FExt = FYes | FNo
+-- | The D (double-precision floating point) extension
 data DExt = DYes | DNo
+-- | The C (compressed instructions) extension
 data CExt = CYes | CNo
-
