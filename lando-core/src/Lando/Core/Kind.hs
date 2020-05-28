2db22434485c78a33dc56e9d595cc2468891dd1a
Ben Selfridge <ben@000548-benselfridge.local>
2020-05-28 15:47:27 -0700

    kind instances and instance validation
    Added an Instance datatype, as well as the necessary types and functions to
    support evaluation of constraints given concrete values for fields, and
    validating instances using those functions

--- lando-core/src/Lando/Core/Kind.hs
+++ lando-core/src/Lando/Core/Kind.hs
@@ -17,6 +17,9 @@ This module defines the core data type for representing a feature model in Lando
 -}
 module Lando.Core.Kind
   ( Kind(..)
+  , Instance(..)
+  , constrainKind
+  , validateInstance
     -- * Fields
   , Field(..)
   , FieldType(..)
@@ -28,10 +31,12 @@ module Lando.Core.Kind
   , riscv
   ) where
 
+import Data.List (find)
 import Data.Parameterized.Classes
 import Data.Parameterized.List
-import Data.Parameterized.Some
 import Data.Parameterized.SymbolRepr
+
+import Prelude hiding ((!!))
 
 -- | Representation of a Lando feature model, which we term a 'Kind' for
 -- brevity.
@@ -41,6 +46,22 @@ data Kind tps = Kind { kindName :: String
                      , kindConstraints :: [Expr tps BoolType]
                      }
   deriving Show
+
+-- | Concrete instance of a kind.
+data Instance tps = Instance { instanceKind :: Kind tps
+                             , instanceValues :: List FieldValue tps
+                             }
+  deriving Show
+
+-- | Create an instance of a kind, checking that all the constraints hold.
+validateInstance :: Kind tps
+                 -> List FieldValue tps
+                 -> Either (Expr tps BoolType) (Instance tps)
+validateInstance kd vals = case find constraintFails (kindConstraints kd) of
+  Just failingConstraint -> Left failingConstraint
+  Nothing -> Right $ Instance kd vals
+  where constraintFails e | BoolLit False <- evalExpr vals e = True
+                          | otherwise = False
 
 -- | A 'Field' is a key, type pair. Note that since both the key and the type
 -- are tracked at the type level, this is a singleton type that lets you know
@@ -58,6 +79,10 @@ data FieldType = BoolType
                | EnumType [Symbol]
                | SetType FieldType
 
+type BoolType = 'BoolType
+type EnumType = 'EnumType
+type SetType = 'SetType
+
 -- | Term-level representative of a field type.
 data FieldTypeRepr tp where
   BoolRepr :: FieldTypeRepr BoolType
@@ -74,6 +99,20 @@ data Literal tp where
 
 deriving instance Show (Literal tp)
 
+eqLit :: Literal tp -> Literal tp -> Bool
+eqLit (BoolLit b1) (BoolLit b2) = b1 == b2
+eqLit (EnumLit _ i1) (EnumLit _ i2) | Just Refl <- i1 `testEquality` i2 = True
+                                    | otherwise = False
+eqLit (SetLit s1) (SetLit s2) = all (\e -> isJust (find (eqLit e) s2)) s1 &&
+                                all (\e -> isJust (find (eqLit e) s1)) s2
+
+-- | Field paired with a literal value of the same type.
+data FieldValue p where
+  FieldValue :: Field '(nm, tp) -> Literal tp -> FieldValue '(nm, tp)
+
+deriving instance Show (FieldValue p)
+instance ShowF FieldValue
+
 -- | An expression built up from literals and fields of a particular kind.
 data Expr (ktps :: [(Symbol, FieldType)]) tp where
   FieldExpr   :: SymbolRepr nm -> Index ktps '(nm, tp) -> Expr ktps tp
@@ -84,8 +123,48 @@ data Expr (ktps :: [(Symbol, FieldType)]) tp where
 
 deriving instance Show (Expr ktps tp)
 
+evalExpr :: List FieldValue ktps -> Expr ktps tp -> Literal tp
+evalExpr vs (FieldExpr _ ix) = let FieldValue _ l = vs !! ix
+                               in l
+evalExpr _ (LiteralExpr l) = l
+evalExpr vs (EqExpr e1 e2) = let l1 = evalExpr vs e1
+                                 l2 = evalExpr vs e2
+                             in BoolLit (eqLit l1 l2)
+evalExpr vs (MemberExpr e1 e2) = let l1 = evalExpr vs e1
+                                     SetLit s2 = evalExpr vs e2
+                                 in BoolLit (isJust (find (eqLit l1) s2))
+evalExpr vs (ImpliesExpr e1 e2) = let BoolLit b1 = evalExpr vs e1
+                                      BoolLit b2 = evalExpr vs e2
+                                  in BoolLit (if b1 then b2 else True)
+
+-- | Add constraints to an existing 'Kind'.
 constrainKind :: [Expr ktps BoolType] -> Kind ktps -> Kind ktps
 constrainKind constraints k = k { kindConstraints = kindConstraints k ++ constraints}
+
+reg_width = Field { fieldName = knownSymbol @"reg_width"
+                  , fieldType = reg_width_type
+                  }
+reg_width_list = knownSymbol @"RV32" :< knownSymbol @"RV64" :< Nil
+reg_width_type = EnumRepr reg_width_list
+
+rv32_val = FieldValue reg_width (EnumLit (knownSymbol @"RV32") index0)
+rv64_val = FieldValue reg_width (EnumLit (knownSymbol @"RV64") index1)
+
+exts = Field { fieldName = knownSymbol @"exts"
+             , fieldType = exts_type
+             }
+exts_list = knownSymbol @"M" :<
+            knownSymbol @"A" :<
+            knownSymbol @"F" :<
+            knownSymbol @"D" :<
+            knownSymbol @"C" :< Nil
+exts_type = SetRepr $ EnumRepr exts_list
+
+m_lit = EnumLit (knownSymbol @"M") index0
+a_lit = EnumLit (knownSymbol @"A") index1
+f_lit = EnumLit (knownSymbol @"F") index2
+d_lit = EnumLit (knownSymbol @"D") index3
+c_lit = EnumLit (knownSymbol @"C") (IndexThere index3)
 
 -- | Feature model for RISC-V ISA (example).
 riscv = Kind { kindName = "riscv"
@@ -93,20 +172,6 @@ riscv = Kind { kindName = "riscv"
              , kindConstraints = [ has_d_implies_has_f ]
              }
   where flds = reg_width :< exts :< Nil
-        reg_width = Field { fieldName = knownSymbol @"reg_width"
-                          , fieldType = reg_width_type
-                          }
-        reg_width_list = knownSymbol @"RV32" :< knownSymbol @"RV64" :< Nil
-        reg_width_type = EnumRepr reg_width_list
-        exts = Field { fieldName = knownSymbol @"exts"
-                     , fieldType = exts_type
-                     }
-        exts_list = knownSymbol @"M" :<
-                    knownSymbol @"A" :<
-                    knownSymbol @"F" :<
-                    knownSymbol @"D" :<
-                    knownSymbol @"C" :< Nil
-        exts_type = SetRepr $ EnumRepr exts_list
 
         d = EnumLit (knownSymbol @"D") index3
         f = EnumLit (knownSymbol @"F") index2
@@ -114,3 +179,9 @@ riscv = Kind { kindName = "riscv"
         has_d = MemberExpr (LiteralExpr d) (FieldExpr (knownSymbol @"exts") index1)
         has_f = MemberExpr (LiteralExpr f) (FieldExpr (knownSymbol @"exts") index1)
         has_d_implies_has_f = ImpliesExpr has_d has_f
+
+my_riscv = validateInstance riscv (rv32_val :< ma :< Nil)
+  where ma = FieldValue exts (SetLit [m_lit, a_lit])
+
+my_bad_riscv = validateInstance riscv (rv64_val :< md :< Nil)
+  where md = FieldValue exts (SetLit [m_lit, d_lit])
