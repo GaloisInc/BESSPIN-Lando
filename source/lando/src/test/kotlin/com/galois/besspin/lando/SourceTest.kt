13d855960db4e6decc0c7426628132070213c9ed
Matthew Yacavone <matthew@yacavone.net>
2020-05-29 18:43:26 -0400

    run one JUnit test per lando source file

--- source/lando/src/test/kotlin/com/galois/besspin/lando/SourceTest.kt
+++ source/lando/src/test/kotlin/com/galois/besspin/lando/SourceTest.kt
@@ -4,56 +4,64 @@ import com.galois.besspin.lando.ssl.ast.RawSSL
 import com.galois.besspin.lando.ssl.ast.rawSSLFromJSON
 import com.galois.besspin.lando.ssl.parser.parseFile
 import kotlinx.serialization.ImplicitReflectionSerializer
+import org.junit.ComparisonFailure
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.ExpectedException
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 import java.io.File
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
+sealed class SourceTestType {
+    object ShouldFail:  SourceTestType()
+    object ShouldParse: SourceTestType()
+    object ShouldWarn:  SourceTestType()
+
+    fun dirName(): String =
+        when (this) {
+            is ShouldFail  -> "should_fail"
+            is ShouldParse -> "should_parse"
+            is ShouldWarn  -> "should_warn"
+        }
+}
+
 //
 //Tests parsing of all .lando source files in the test/lando directory,
 //in the style of the Glasgow Haskell Compiler's (GHC) test framework.
 //
-class SourceTest {
+@RunWith(Parameterized::class)
+class SourceTest(
+    private val source: File,
+    private val sourceString: String,
+    private val testType: SourceTestType
+) {
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters(name="{1}")
+        fun tests() : Iterable<Array<Any>> {
+            val testTypesToRun: List<SourceTestType> =
+                listOf(SourceTestType.ShouldFail, SourceTestType.ShouldParse, SourceTestType.ShouldWarn)
 
-    private sealed class SourceTestType {
-        object ShouldFail:  SourceTestType()
-        object ShouldParse: SourceTestType()
-        object ShouldWarn:  SourceTestType()
-
-        fun dirName(): String =
-            when (this) {
-                is ShouldFail  -> "should_fail"
-                is ShouldParse -> "should_parse"
-                is ShouldWarn  -> "should_warn"
+            val toTest: MutableList<Array<Any>> = mutableListOf()
+            for (topDir in File("src/test/lando/").listFiles()!!) {
+                for (testType in testTypesToRun) {
+                    for (file in File(topDir, testType.dirName()).walkTopDown()) {
+                        if (file.extension == "lando")
+                            toTest.add(arrayOf(file, file.path.removePrefix("src/test/lando/"), testType))
+                    }
+                }
             }
+            return toTest
+        }
     }
-    private val testTypesToRun: List<SourceTestType> =
-        listOf(SourceTestType.ShouldFail, SourceTestType.ShouldParse, SourceTestType.ShouldWarn)
 
     @Test
     @ImplicitReflectionSerializer
-    fun testAll() {
-        val toTest: MutableList<Pair<File, SourceTestType>> = mutableListOf()
-        for (topDir in File("src/test/lando/").listFiles()!!) {
-            for (testType in testTypesToRun) {
-                for (file in File(topDir, testType.dirName()).walkTopDown()) {
-                    if (file.extension == "lando")
-                        toTest.add(Pair(file, testType))
-                }
-            }
-        }
-
-        println("Going to test ${toTest.size} lando source files.")
-        for ((file, testType) in toTest) {
-            println(file.path)
-            testFile(file, testType)
-        }
-    }
-
-    @ImplicitReflectionSerializer
-    private fun testFile(source: File, testType: SourceTestType) {
-        assertEquals("lando", source.extension, "${source.path} is not a lando file")
+    fun testFile() {
+        assertEquals("lando", source.extension, "is not a lando file")
 
         var errorMsg: String? = null; var warnMsg: String? = null; var ast: RawSSL? = null
         try {
@@ -70,25 +78,23 @@ class SourceTest {
 
         when (testType) {
             is SourceTestType.ShouldFail -> {
-                assertNotNull(errorMsg, "${source.path} did not generate any errors")
-                assertTrue(errFile.exists(), "${errFile.path} does not exist")
-                assertEquals(errFile.readText().trim(), errorMsg, "${source.path} error mismatch:\n")
+                assertNotNull(errorMsg, "did not generate any errors")
+                assertTrue(errFile.exists(), "does not exist")
+                assertEquals(errFile.readText().trim(), errorMsg, "error mismatch:\n")
             }
             is SourceTestType.ShouldWarn -> {
-                assertNotNull(warnMsg, "${source.path} did not generate any warnings")
-                assertTrue(warnFile.exists(), "${warnFile.path} does not exist")
-                assertEquals(warnFile.readText().trim(), warnMsg, "${source.path} warnings mismatch:\n")
+                assertNotNull(warnMsg, "did not generate any warnings")
+                assertTrue(warnFile.exists(), "does not exist")
+                assertEquals(warnFile.readText().trim(), warnMsg, "warnings mismatch:\n")
                 //the below is identical to that in the ShouldParse case
-                assertNotNull(ast, "${source.path} did not parse")
-                assertTrue(jsonFile.exists(), "${jsonFile.path} does not exist")
-                assertEquals(rawSSLFromJSON(jsonFile.readText().trim()), ast,
-                             "${source.path} does not math ${jsonFile.path}:\n")
+                assertNotNull(ast, "did not parse")
+                assertTrue(jsonFile.exists(), "does not exist")
+                assertEquals(rawSSLFromJSON(jsonFile.readText().trim()), ast, "does not match JSON:\n")
             }
             is SourceTestType.ShouldParse -> {
-                assertNotNull(ast, "${source.path} did not parse")
-                assertTrue(jsonFile.exists(), "${jsonFile.path} does not exist")
-                assertEquals(rawSSLFromJSON(jsonFile.readText().trim()), ast,
-                    "${source.path} does not math ${jsonFile.path}:\n")
+                assertNotNull(ast, "did not parse")
+                assertTrue(jsonFile.exists(), "does not exist")
+                assertEquals(rawSSLFromJSON(jsonFile.readText().trim()), ast, "does not math JSON:\n")
             }
         }
     }
