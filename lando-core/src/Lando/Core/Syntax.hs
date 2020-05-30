8c082653651edcfb2cd089a4dd1189ab44671ea3
Ben Selfridge <ben@000548-benselfridge.local>
2020-05-30 13:36:01 -0700

    Worked on untyped AST a bit

--- lando-core/src/Lando/Core/Syntax.hs
+++ lando-core/src/Lando/Core/Syntax.hs
@@ -4,28 +4,43 @@
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeApplications #-}
 
+{-|
+Module      : Lando.Core.Syntax
+Description : The untyped AST for the Lando LOBOT sublanguage.
+Copyright   : (c) Ben Selfridge, 2020
+License     : BSD3
+Maintainer  : benselfridge@galois.com
+Stability   : experimental
+Portability : POSIX
+
+This module defines the untyped AST for the LOBOT sublanguage of lando. When
+parsing a concrete syntax for Lando, this is the data type we target. Then, this
+type passes through the type checker to produce the typed AST in
+'Lando.Core.Kind'.
+-}
 module Lando.Core.Syntax where
 
-data FieldDecl = FieldDecl FieldName FieldType
-
-data FieldName = FieldName String
-
-data FieldType = BaseType BaseType
-
-data BaseType = IntType
-              | EnumType [String]
-              | SubsetType BaseType
-
-data Expr = Literal Literal
-          | ExprField String
-          | Equal Expr Expr
-          | Set [Expr]
-
+-- | Untyped AST for a LOBOT Kind.
 data Kind = Kind { kindName :: String
                  , kindParent :: Maybe String
                  , kindFields :: [FieldDecl]
                  , kindConstraints :: [Expr]
                  }
+
+-- | Untyped field declaration.
+data FieldDecl = FieldDecl FieldName FieldType
+
+-- | Name of a field.
+data FieldName = FieldName String
+
+data FieldType = IntType
+               | EnumType [String]
+               | SubsetType BaseType
+
+data Expr = Literal Literal
+          | ExprField String
+          | Equal Expr Expr
+          | Set [Expr]
 
 data Instance = Instance { instanceName :: String
                          , instanceKind :: String
