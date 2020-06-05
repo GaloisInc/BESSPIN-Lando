bae73a89f253d2fbdb1102e86298306972d980aa
Ben Selfridge <ben@000548-benselfridge.local>
2020-06-04 22:18:19 -0700

    Fixes up examples a bit

--- lando-core/src/Lando/Core/Kind/Examples.hs
+++ lando-core/src/Lando/Core/Kind/Examples.hs
@@ -15,8 +15,8 @@ module Lando.Core.Kind.Examples
     -- * Example 3
   , RegWidthType, ExtsType, RISCVType
   , riscv
-  , PiccoloBuildType
-  , piccolo_build
+  , SimType, BluespecBuildType
+  , bluespec_build
   ) where
 
 import Data.List.NonEmpty
@@ -27,7 +27,7 @@ import Lando.Core.Kind
 
 type ABCType = '["A", "B", "C"]
 
-type FooType = '[ '("abc", SetType ABCType ) ]
+type FooType = '[ '("abc", SetType ABCType) ]
 
 abc_type :: List SymbolRepr ABCType
 abc_type = knownRepr
@@ -80,7 +80,7 @@ abc_kind_2 = derivedKind (abc_kind :| []) "abc_kind_2"
 abc_kind_3 :: Kind FooType
 abc_kind_3 = derivedKind
   (abc_kind_1 :| [abc_kind_2])
-  "abc_kind_1_and_A_implies_C"
+  "abc_kind_3"
   []
 
 type SexType = '["Male", "Female"]
@@ -164,175 +164,22 @@ type SimType = '[ "Bluesim", "IVerilog", "Verilator" ]
 sim_type :: List SymbolRepr SimType
 sim_type = knownRepr
 
-type PiccoloBuildType = '[ '("riscv", KindType RISCVType)
-                         , '("sim", EnumType SimType)
-                         ]
+type BluespecBuildType = '[ '("riscv", KindType RISCVType)
+                          , '("sim", EnumType SimType)
+                          , '("INCLUDE_TANDEM_VERIF", BoolType)
+                          ]
+
 -- |
 -- @
--- kind piccolo_build
+-- kind bluespec_build
 --   with riscv : riscv,
 --        sim : {Bluesim, IVerilog, Verilator}
 -- @
-piccolo_build :: Kind PiccoloBuildType
-piccolo_build = liftConstraints k riscv index0
-  where k = Kind { kindName = "piccolo_build"
+bluespec_build :: Kind BluespecBuildType
+bluespec_build = liftConstraints k riscv index0
+  where k = Kind { kindName = "bluespec_build"
                  , kindFields = FieldRepr knownSymbol (KindRepr (kindFields riscv)) :<
-                                FieldRepr knownSymbol (EnumRepr sim_type) :< Nil
+                                FieldRepr knownSymbol (EnumRepr sim_type) :<
+                                FieldRepr knownSymbol BoolRepr :< Nil
                  , kindConstraints = []
                  }
-
--- couple :: Kind '[ '("person1", KindType PersonType)
---                 , '("person2", KindType PersonType) ]
--- couple = Kind { kindName = "couple"
---               , kindFields = FieldRepr (knownSymbol @"person1") personRepr :<
---                              FieldRepr (knownSymbol @"person2") personRepr :< Nil
---               , kindConstraints = []
---               }
---   where personRepr = KindRepr (age :< sex :< Nil)
-
--- impossible_person :: Kind PersonType
--- impossible_person = derivedKind person "impossible_person "
---   [ EqExpr
---     (FieldExpr SelfExpr index1) -- sex
---     (LiteralExpr (EnumLit sex_type index0)) -- male
---   , EqExpr
---     (FieldExpr SelfExpr index1) -- sex
---     (LiteralExpr (EnumLit sex_type index1)) -- female
---   ]
-
--- male_person :: Kind PersonType
--- male_person = derivedKind person "male_person"
---   [ EqExpr
---     (FieldExpr SelfExpr index1) -- sex
---     (LiteralExpr (EnumLit sex_type index0)) -- male
---   ]
-
--- female_person :: Kind PersonType
--- female_person = person `addConstraints`
---   [ EqExpr
---     (FieldExpr SelfExpr index1) -- sex
---     (LiteralExpr (EnumLit index1 knownNat)) -- female
---   ]
-
--- child :: Kind PersonType
--- child = person `addConstraints`
---   [ LteExpr (FieldExpr SelfExpr index0) (LiteralExpr (IntLit 18))]
-
--- ben :: Instance PersonType
--- ben = Instance { instanceValues =
---                  FieldLiteral knownSymbol (IntLit 30) :<
---                  FieldLiteral knownSymbol (EnumLit sex_type index0) :< Nil
---                }
-
--- emmy :: Instance PersonType
--- emmy = Instance { instanceValues =
---                   FieldLiteral age (IntLit 8) :<
---                   FieldLiteral sex (EnumLit index1 knownNat) :< Nil
---                 }
-
--- archer :: Instance PersonType
--- archer = Instance { instanceValues =
---                     FieldLiteral age (IntLit 4) :<
---                     FieldLiteral sex (EnumLit index0 knownNat) :< Nil
---                   }
-
-
--- reg_width :: FieldRepr '("reg_width", EnumType '["RV32", "RV64"])
--- reg_width = FieldRepr
---   { fieldName = knownSymbol @"reg_width"
---   , fieldType = EnumRepr (knownSymbol @"RV32" :< knownSymbol @"RV64" :< Nil) knownNat
---   }
-
--- exts :: FieldRepr '("exts", SetType '["M", "A", "F", "D", "C"])
--- exts = FieldRepr
---   { fieldName = knownSymbol @"exts"
---   , fieldType = SetRepr (knownSymbol @"M" :<
---                          knownSymbol @"A" :<
---                          knownSymbol @"F" :<
---                          knownSymbol @"D" :<
---                          knownSymbol @"C" :< Nil)
---                 knownNat
---   }
-
--- riscv_with_m :: Kind RISCVType
--- riscv_with_m = riscv `addConstraints`
---   [ MemberExpr
---     (LiteralExpr (EnumLit index0 knownNat)) -- M
---     (FieldExpr SelfExpr index1) -- exts
---   ]
-
--- riscv_with_only_d :: Kind RISCVType
--- riscv_with_only_d = riscv `addConstraints`
---   [ EqExpr
---     (FieldExpr SelfExpr index1) -- exts
---     (LiteralExpr (SetLit [Some index3] knownNat)) -- D
---   ]
-
--- rv32i :: Instance RISCVType
--- rv32i = Instance { instanceValues =
---                    FieldLiteral reg_width (EnumLit index0 knownNat) :<
---                    FieldLiteral exts (SetLit [] knownNat) :<
---                    Nil
---                  }
-
--- rv32m :: Instance RISCVType
--- rv32m = Instance { instanceValues =
---                    FieldLiteral reg_width (EnumLit index0 knownNat) :<
---                    FieldLiteral exts (SetLit [Some index0] knownNat) :<
---                    Nil
---                  }
-
--- rv32d :: Instance RISCVType
--- rv32d = Instance { instanceValues =
---                    FieldLiteral reg_width (EnumLit index0 knownNat) :<
---                    FieldLiteral exts (SetLit [Some index3] knownNat) :<
---                    Nil
---                  }
-
--- rv64gc :: Instance RISCVType
--- rv64gc = Instance { instanceValues =
---                     FieldLiteral reg_width (EnumLit index1 knownNat) :<
---                     FieldLiteral exts (SetLit [ Some index0
---                                             , Some index1
---                                             , Some index2
---                                             , Some index3
---                                             , Some (IndexThere index3)
---                                             ] knownNat) :<
---                     Nil
---                   }
-
--- riscv_field :: FieldRepr '("riscv", KindType RISCVType)
--- riscv_field = FieldRepr { fieldName = knownSymbol @"riscv"
---                         , fieldType = KindRepr (reg_width :< exts :< Nil)
---                         }
-
--- sim :: FieldRepr '("sim", EnumType '["Bluesim", "IVerilog", "Verilator"])
--- sim = FieldRepr { fieldName = knownSymbol @"sim"
---                 , fieldType = EnumRepr (knownSymbol @"Bluesim" :<
---                                         knownSymbol @"IVerilog" :<
---                                         knownSymbol @"Verilator" :<
---                                         Nil)
---                               knownNat
---                 }
-
--- type PiccoloBuildType = '[ '("riscv", KindType RISCVType)
---                          , '("sim", EnumType '["Bluesim", "IVerilog", "Verilator"])
---                          ]
-
--- piccolo_build :: Kind PiccoloBuildType
--- piccolo_build = Kind { kindName = "piccolo_build"
---                      , kindFields = riscv_field :< sim :< Nil
---                      , kindConstraints =
---                          [ ImpliesExpr -- inherited from riscv
---                            (MemberExpr -- D in riscv.exts
---                             (LiteralExpr (EnumLit index3 knownNat)) -- riscv.D
---                             (FieldExpr -- riscv.exts
---                              (FieldExpr SelfExpr index0) -- riscv
---                              index1))
---                            (MemberExpr -- F in riscv.exts
---                             (LiteralExpr (EnumLit index2 knownNat)) -- riscv.F
---                             (FieldExpr -- riscv.exts
---                              (FieldExpr SelfExpr index0) -- riscv
---                              index1))
---                          ]
---                      }
