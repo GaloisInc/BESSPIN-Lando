953f51c9bc01db48622f7fc01fa5ce12bcebf985
Ben Selfridge <ben@000548-benselfridge.local>
2020-06-14 11:15:35 -0700

    Elaborates tests
    Now, tests compute every satisfying instance up to 2000 and compare against the
    golden output.

--- lobot-core/tests/Tests.hs
+++ lobot-core/tests/Tests.hs
@@ -10,20 +10,24 @@ import qualified Data.ByteString.Lazy.Char8 as LBS
 import Data.Parameterized.Classes
 import Data.Parameterized.Context hiding (last)
 import Data.Parameterized.Some
+import Numeric.Natural
 import System.Exit (exitFailure)
 import System.FilePath (takeBaseName, replaceExtension)
 import Test.Tasty (defaultMain, TestTree, testGroup)
 import Test.Tasty.Golden (goldenVsString, findByExtension)
 import Text.Show.Pretty (ppShow)
 
-countLimit :: Integer
+countLimit :: Natural
 countLimit = 2000
+
+z3 :: String
+z3 = "/usr/local/bin/z3"
 
 main :: IO ()
 main = defaultMain =<< goldenTests
 
 data TestResult = TestResult { lastKind :: Some (Kind EmptyCtx)
-                             , numInstances :: Integer
+                             , instances :: [Some Literal]
                              }
   deriving Show
 
@@ -40,8 +44,8 @@ testLobotFile fileName = do
                      exitFailure
       Right ks -> case last ks of
         Some k -> do
-          n <- countInstances countLimit "/usr/local/bin/z3" Empty k
-          return $ TestResult (Some k) n
+          insts <- collectInstances z3 Empty k countLimit
+          return $ TestResult (Some k) (Some <$> insts)
 
 goldenTests :: IO TestTree
 goldenTests = do
